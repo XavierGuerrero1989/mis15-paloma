a2a729f194b1c0a2ff98f33048d287e7288c2644
Xavier Ignacio Guerrero <xavierignacioguerrero@Mac.fibertel.com.ar>
2026-01-15 23:21:06 -0300

    nuevo intento

--- src/sections/DresscodeRsvp.jsx
+++ src/sections/DresscodeRsvp.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 import RsvpModal from "../components/RsvpModal";
-import "../sections/dresscode.css";
+import "../sections/DressCode.css";
 
 export default function Dresscode() {
   const { guestSlug } = useParams();
